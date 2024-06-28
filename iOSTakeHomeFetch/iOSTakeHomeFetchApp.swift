d03aa30e944ae46983fbc2e4787a6604bb2e4055
Dev-Steven <stevenchung650@gmail.com>
2024-06-28 07:52:27 -0700

    Initial Commit

---
+++ iOSTakeHomeFetch/iOSTakeHomeFetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTakeHomeFetchApp.swift
+//  iOSTakeHomeFetch
+//
+//  Created by Steven Chung on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSTakeHomeFetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
